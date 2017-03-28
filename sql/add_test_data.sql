6427487ff5369bef118c9f206cdfb79c49ad5a22
Matti Känninen <mattikan@cs.helsinki.fi>
2017-03-28 14:21:20 +0300

    rest of last week

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,35 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Location (name, description) VALUES ('Alina', 'Uudella sijaitseva tilava juhlasali.');
+INSERT INTO Location (name, description) VALUES ('Klusteri', 'Se tuttu ja turvallinen(?).');
+
+INSERT INTO Player (name, password) VALUES ('nakki', 'nakki123');
+INSERT INTO Player (name, password) VALUES ('kjarkko', 'roskis');
+
+INSERT INTO Game (confirmed, location_id, winning_team) values (true, (select id from Location where name like 'Alina'), 0);
+
+INSERT INTO Game (confirmed, location_id, winning_team) values (true, (select id from Location where name like 'Klusteri'), 1);
+
+
+INSERT INTO Participation (player_id, game_id, team) values (
+	(select id from Player where name like 'nakki'),
+	(select id from Game where winning_team = 0),
+	0
+	);
+
+INSERT INTO Participation (player_id, game_id, team) values (
+	(select id from Player where name like 'nakki'),
+	(select id from Game where winning_team = 1),
+	1
+	);
+
+INSERT INTO Participation (player_id, game_id, team) values (
+	(select id from Player where name like 'kjarkko'),
+	(select id from Game where winning_team = 0),
+	1
+	);
+
+INSERT INTO Participation (player_id, game_id, team) values (
+	(select id from Player where name like 'kjarkko'),
+	(select id from Game where winning_team = 1),
+	0
+	);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,27 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Location(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	description text
+);
+
+CREATE TABLE Player(
+	id SERIAL PRIMARY KEY,
+	name varchar(50) NOT NULL,
+	password varchar(50) NOT NULL
+);
+
+CREATE TABLE Game(
+	id SERIAL PRIMARY KEY,
+	confirmed boolean DEFAULT FALSE,
+	played DATE,
+	location_id INTEGER REFERENCES Location(id) ON UPDATE CASCADE,
+	added DATE,
+	winning_team INTEGER
+);
+
+CREATE TABLE Participation(
+	player_id INTEGER REFERENCES Player(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	game_id INTEGER REFERENCES Game(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	team INTEGER
+);
